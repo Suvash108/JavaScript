f4add52bc4458f45c94e8c8c2f238d7ac8299c25
Suvash Chandra Yadav <70757327+Suvash108@users.noreply.github.com>
2025-03-26 09:38:03 +0000

    data type conversion prefix and postfix

---
+++ 01basics/conversion.js
@@ -0,0 +1,26 @@
+// let score = 33 
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// "33"=>33
+// "33abc"=>NaN->not a number
+//"true->1 and false->0"
+
+// let loggedin =1
+// let bloggedin= Boolean(loggedin);
+// console.log(typeof bloggedin);
+// console.log(bloggedin);
+
+
+// // ""=>false
+// let name=99;
+// let sname=String(name);
+// console.log(sname)
+// console.log(typeof sname);
+
+
+let x= 5;
+console.log(x)
+let y = ++x;
+// console.log(x,y)
+console.log('x:${x}');
