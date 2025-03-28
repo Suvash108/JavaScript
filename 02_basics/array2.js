881ceb4cb853ac36691df1b43d9a65b96bdc0e2a
Suvash Chandra Yadav <70757327+Suvash108@users.noreply.github.com>
2025-03-28 10:16:39 +0000

    sigma,flat concat

---
+++ 02_basics/array2.js
@@ -0,0 +1,13 @@
+
+const sigma=["suvash","Suvash", "Ssuvasshhhh"]
+const bhagwan=["Srichaitanya", "ramayan","Bholenath"]
+const all=[...sigma,...bhagwan]
+console.log(all)
+
+var golmall= [1,23,323,[2,3,4,5,5,6,6,[33,44,553,55323423,5,,32,3,54,23[7,5,4,64,4,34,6,43,45,34,53,45,32,4,425,43534534536,6,6]]]]
+console.log(golmall.flat(Infinity))
+
+console.log(Array.from("Suvashdada"))
+
+var conacateee= sigma.concat(bhagwan)
+console.log(conacateee)
