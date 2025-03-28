b10fddc3ff1687cacdda4598d7b1a547eaa0b0dd
Suvash Chandra Yadav <70757327+Suvash108@users.noreply.github.com>
2025-03-28 06:46:58 +0000

    some additional hands on using Math. function

--- 01basics/numb.js
+++ 01basics/numb.js
@@ -1,3 +1,8 @@
-const balance = new Number (400)
-console.log(balance);
-console.log(balance.toString().length)
+// const balance = new Number (400)
+// console.log(balance);
+// console.log(balance.toString().length)
+
+console.log(Math.abs(-233))
+console.log(Math.round(8.9))
+console.log(Math.floor(8.9));
+console.log(Math.ceil(8.4))
