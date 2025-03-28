14fa1e29948c01a4c5f8dfe5f5b29b9e06d2a59b
Suvash Chandra Yadav <70757327+Suvash108@users.noreply.github.com>
2025-03-28 07:18:36 +0000

    date and the basics learnt

---
+++ 01basics/date.js
@@ -0,0 +1,8 @@
+
+let mydate = new Date()
+console.log(mydate.toLocaleDateString())
+console.log(Date.now())
+console.log(Math.floor(Date.now()/1000))// this gives us a date in second
+
+const mybirthday = new Date(2000-1-12)
+console.log(mybirthday)
