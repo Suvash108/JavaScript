24fba4c22757a42f25c839cec247d8fcdce2113e
Suvash Chandra Yadav <70757327+Suvash108@users.noreply.github.com>
2025-03-30 13:30:17 +0000

    rest operation on using shopping cart

--- function.js/basic.js
+++ function.js/basic.js
@@ -3,7 +3,7 @@ function printmyname()
     console.log("congratulations Suvash you are a js master now")
 }
 
-//console.log(printmyname());
+console.log(printmyname());
 // printmyname-> this is  just a reference
 // printmyname()->this a function
 
@@ -14,12 +14,18 @@ function add(number1,number2)
 {
     return number1+number2
 }
-// console.log(add(6,"Suvasha"));
-//here suvash and 6 are arguments 
-// console.log("suvash", 44);
+console.log(add(6,"Suvasha"));
+// here suvash and 6 are arguments 
+console.log("suvash", 44);
 
 
 
 
 //--------------------------------------------------------------------------------------------------
- 
+ function cartpricecalculation(...num1)
+ {
+    return num1
+
+ }
+
+ console.log(cartpricecalculation(100,2000,3500,4234,833))
