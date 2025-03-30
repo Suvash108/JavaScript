4e72783aef357aecc159de15618dfcf491379d5b
Suvash Chandra Yadav <70757327+Suvash108@users.noreply.github.com>
2025-03-30 12:36:02 +0000

    function basic

---
+++ function.js/basic.js
@@ -0,0 +1,25 @@
+function printmyname()
+{
+    console.log("congratulations Suvash you are a js master now")
+}
+
+//console.log(printmyname());
+// printmyname-> this is  just a reference
+// printmyname()->this a function
+
+//printmyname()
+
+function add(number1,number2) 
+//here number1 and number2 are parameters 
+{
+    return number1+number2
+}
+// console.log(add(6,"Suvasha"));
+//here suvash and 6 are arguments 
+// console.log("suvash", 44);
+
+
+
+
+//--------------------------------------------------------------------------------------------------
+ 
