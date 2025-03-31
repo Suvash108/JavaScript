4e183cdec21b2672abdffe6b9114658024c19850
Suvash Chandra Yadav <70757327+Suvash108@users.noreply.github.com>
2025-03-31 13:43:50 +0000

    layer 2 of understanding

--- function.js/scope..js
+++ function.js/scope..js
@@ -6,6 +6,25 @@ if(true)
 }
 //console.log(v); Here it is throwing some errors and why is this ? because of scope. so it means that let and consts are  local scope and howerver var is a global scope var delcaration method. 
 //console.log(vi)
-console.log(k)
+//console.log(k)
 
 //------------------------------------------------------------------------------------------------------------------------------------
+
+
+function one ()
+{
+    let username="Adarsh ";
+    let usercaste="Mishra "
+
+    function two()
+    {
+        console.log(username+usercaste);
+        let king="suvash";
+        console.log(king )
+    }
+
+    two()
+    //console.log(king) defined outside the block 
+
+}
+one()
