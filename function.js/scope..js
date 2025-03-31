deb77159b64787513d92e0c0297550746473b4ab
Suvash Chandra Yadav <70757327+Suvash108@users.noreply.github.com>
2025-03-31 13:21:56 +0000

    basics about local and global completed

---
+++ function.js/scope..js
@@ -0,0 +1,11 @@
+if(true)
+{
+    let v="Suvash"
+    const vi="version"
+    var k="lels"
+}
+//console.log(v); Here it is throwing some errors and why is this ? because of scope. so it means that let and consts are  local scope and howerver var is a global scope var delcaration method. 
+//console.log(vi)
+console.log(k)
+
+//------------------------------------------------------------------------------------------------------------------------------------
