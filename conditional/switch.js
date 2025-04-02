c58daf20022420936c5bd8edbf8122aa7ba4f837
Suvash Chandra Yadav <70757327+Suvash108@users.noreply.github.com>
2025-04-02 13:28:10 +0000

    switch bhi hogaya

---
+++ conditional/switch.js
@@ -0,0 +1,46 @@
+// Prompt the user to enter the month number
+
+month = Number(4);
+
+// Use a switch statement to determine the month
+switch (month) {
+    case 1:
+        console.log("January it is!");
+        break;
+    case 2:
+        console.log("February it is!");
+        break;
+    case 3:
+        console.log("March it is!");
+        break;
+    case 4:
+        console.log("April it is!");
+        break;
+    case 5:
+        console.log("May it is!");
+        break;
+    case 6:
+        console.log("June it is!");
+        break;
+    case 7:
+        console.log("July it is!");
+        break;
+    case 8:
+        console.log("August it is!");
+        break;
+    case 9:
+        console.log("September it is!");
+        break;
+    case 10:
+        console.log("October it is!");
+        break;
+    case 11:
+        console.log("November it is!");
+        break;
+    case 12:
+        console.log("December it is!");
+        break;
+    default:
+        console.log("Invalid month number, please enter a number between 1 and 12.");
+        break;
+}
